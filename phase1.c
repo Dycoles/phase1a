d7b8ec58428d1a40f93b10ed4fcb0cf854c5f36d
Dycoles <134562267+Dycoles@users.noreply.github.com>
2025-02-08 12:03:26 -0700

    Create phase1.c

---
+++ phase1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+// #include <phase1.h>
+
+int main() {
+    return 1;
+}
+
+void phase1_init(void) {
+// should create testcase_main
+// USLOSS_Halt(0)
+}
+
+
+int spork(char *name, int (*startFunc)(void), void *arg, int stackSize, int priority) {
+    return 1;
+}
+
+int join(int *status) {
+
+}
+
+void quit_phase_1a(int status, int switchToPid) {
+
+}
+
+int getpid(void) {
+
+}
+
+void dumpProcesses(void) {
+
+}
+
+void TEMP_switchTo(int newpid) {
+// USLOSS_ContextSwitch
+}
+
+//void zap(int pid)
+
+//void blockMe()
+
+//int unblockProc(int pid)
