cabc04cdefaa16d851d3f2f4c46fc1fdd4611957
jackmwilliams <166460615+jackmwilliams@users.noreply.github.com>
2025-02-10 14:49:38 -0700

    Basic phase1_init structure

--- phase1.c
+++ phase1.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 // #include <phase1.h>
 
+struct process {
+    int PID;
+    int priority;
+    char[MAXNAME+1] name;
+};
+
+struct process[MAXPROC] processes;
+
 int main() {
     return 1;
 }
 
 void phase1_init(void) {
+    processes[0]->PID = 1;
+    processes[0]->priority = 6;
+    processes[0]->name = "init";
+
+    spork("testcase_main", (*testcase_main), NULL, sizeof(process), 3);
+
+    int curProcess = 1;
+    int status;
+    while (1) {
+        if (join(&status) == -2) {
+            return; // unsure if this is how to quit
+        }
+        curProcess = (curProcess+1)%MAXPROC;
+    }
+
 // should create testcase_main
 // USLOSS_Halt(0)
 }
