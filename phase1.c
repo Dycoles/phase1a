6df43863c60e71ae1bbf3ea1bba42e742e411c37
Dycoles <134562267+Dycoles@users.noreply.github.com>
2025-02-10 14:54:55 -0700

    Update phase1.c
    comments

--- phase1.c
+++ phase1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-// #include <phase1.h>
+#include <phase1.h>
 
 struct process {
     int PID;
@@ -8,7 +8,7 @@ struct process {
 };
 
 struct process[MAXPROC] processes;
-
+    
 int main() {
     return 1;
 }
@@ -28,30 +28,38 @@ void phase1_init(void) {
         }
         curProcess = (curProcess+1)%MAXPROC;
     }
-
 // should create testcase_main
-// USLOSS_Halt(0)
+// set up process table
+// once testcase_main created, call ULOSS_ContextSwitch()
+// USLOSS_Halt(0) if testcase_main ever returns
+// if any other function returns, print an error and terminiate
 }
 
 
 int spork(char *name, int (*startFunc)(void), void *arg, int stackSize, int priority) {
+    // creates new processs as a child of the current process, assigns it a pid, and stores it in the process table
+    // return error if the table is full
     return 1;
 }
 
 int join(int *status) {
-
+    // waits for child (works like wait in Unix)
+    // process pauses until one of its children quits
+    // receives child's exit status
 }
 
 void quit_phase_1a(int status, int switchToPid) {
-
+// if error Usloss halt (works like exit in UNIX)
+// ends the currrent process but keeps its entry in the process table until the parent calls join
+// if parent waiting, wakes up
 }
 
 int getpid(void) {
-
+// returns the PID of cur process
 }
 
 void dumpProcesses(void) {
-
+// prints debug info about process table
 }
 
 void TEMP_switchTo(int newpid) {
