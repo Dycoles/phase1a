bf6c3a54733c83e416473a1e8db6abed91d67f48
Dycoles <134562267+Dycoles@users.noreply.github.com>
2025-02-13 17:15:35 -0700

    Update phase1.c

--- phase1.c
+++ phase1.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <phase1.h>
+#include <string.h>
+#include <usloss.h>
+// temp variables to prevent errors
+USLOSS_MIN_STACK = 50;
 
 struct process {
     int PID;
     int priority;
     char name[MAXNAME + 1];
+    struct process *parent;
+    struct process *next_sibling;
+    struct process *first_child;
+    // flag is 1 if in use, 0 if unused
+    int in_use;
+
+    // commented out to get rid of errors for now; this is necessary code
+
+    // USLOSS_Context context;
 };
 
 int j = 0;
+int currentPID = 0;
 
 struct process process_table[MAXPROC];
     
@@ -16,8 +30,11 @@ int main() {
 }
 
 void phase1_init(void) {
+    memset(process_table, 0, sizeof(process_table));
+
     process_table[0].PID = 1;
     process_table[0].priority = 6;
+    process_table[0].in_use = 1;
     strcpy(process_table[0].name, "init");
     
     spork("testcase_main", (*testcase_main), NULL, sizeof(process_table), 3);
@@ -26,6 +43,9 @@ void phase1_init(void) {
     int status;
     while (1) {
         if (join(&status) == -2) {
+            // print out an error message here
+            // call USLOSS_halt
+            USLOSS_halt(-2);
             return; // unsure if this is how to quit
         }
         curProcess = (curProcess+1)%MAXPROC;
@@ -39,16 +59,42 @@ void phase1_init(void) {
 
 
 int spork(char *name, int (*startFunc)(void), void *arg, int stackSize, int priority) {
+    // if stack size smaller than USLOSS_MIN_STACK, return -2
+    if (stackSize < USLOSS_MIN_STACK) {
+        return -2;
+    }
+    // if name or startFunc is null, priority is out of range, or name is longer than MAXNAME, return -1
+    if (name == NULL || startFunc == NULL || priority < 1 || priority > 5 || strlen(name) >= MAXNAME) {
+        return -1;
+    }
+    // if no empty slots in the process table return -1; -> This part may be incorrect. May need to change how we do our struct/queue
     int slot = getpid() % MAXPROC;
+    if (process_table[slot].in_use) {
+        return -1;
+    }
+
+    // fill the table and assign it as a child of the previous process -> This part is probably incorrect, may need to change how we do our struct/queue
     process_table[slot].PID = 1;
     process_table[slot].priority = priority;
+    process_table[slot].in_use = 1;
     strcpy(process_table[0].name, name);
-    // creates new processs as a child of the current process, assigns it a pid, and stores it in the process table
-    // return error if the table is full
-    return 1;
+
+    // USLOSS_ContextInit(USLOSS_Context *context, void *stack, int stackSize, USLOSS_PTE *pageTable, void(*func)(void)) -> syntax for context init
+    // return PID of the child process
+    return process_table[slot].PID;
 }
 
 int join(int *status) {
+    // if argument is invalid, return -3
+    if (status == NULL) {
+        return -3;
+    }
+    for (int i = 0; i < MAXPROC; i++) {
+        // if child exists, return PID of the child
+    }
+    // return -2 if the process does not have any children
+    return -2;
+
     // waits for child (works like wait in Unix)
     // process pauses until one of its children quits
     // receives child's exit status
@@ -62,6 +108,7 @@ void quit_phase_1a(int status, int switchToPid) {
 
 int getpid(void) {
 // returns the PID of cur process
+return currentPID;
 }
 
 void dumpProcesses(void) {
@@ -69,7 +116,12 @@ void dumpProcesses(void) {
 }
 
 void TEMP_switchTo(int newpid) {
-// USLOSS_ContextSwitch
+    // USLOSS_ContextSwitch
+    int oldPID = currentPID;
+    currentPID = newpid;
+
+    // USLOSS_ContextSwitch(USLOSS_Context *old_context, USLOSS_Context *new_context) -> syntax for context swtiching
+    // USLOSS_ContextSwitch(process_table[oldPID].context, process_table[newpid].context); -> code for context switching, commented out for now
 }
 
 //void zap(int pid)
