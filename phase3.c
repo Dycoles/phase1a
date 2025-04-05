6111ec454fa0843a102b80c511beaa05d88c2590
jackmwilliams <166460615+jackmwilliams@users.noreply.github.com>
2025-04-04 17:09:20 -0700

    Added block comments

--- phase3.c
+++ phase3.c
@@ -1,3 +1,13 @@
+/*
+ * Phase 3 (phase3.c)
+ * University of Arizona
+ * CSC 452
+ * 04/04/2025
+ * Authors: 
+ *     Dylan Coles
+ *     Jack Williams
+*/
+
 #include <usloss.h>
 #include <usyscall.h>
 #include <phase1.h>
@@ -11,6 +21,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+ * The Semaphore struct tracks the value of a semaphore, along with its
+ * queue of blocked processes if any try to P() when the value is 0.
+ */
 typedef struct {
     int value;
     int numBlockedProcs;
@@ -19,6 +33,10 @@ typedef struct {
     int back;
 } Semaphore;
 
+/*
+ * The trampolineFunc struct wraps a user-mode function in a trampoline,
+ * allowing it to be called in user mode as it should.
+ */
 typedef struct {
     int trampID;
     int(*func)(void *);
@@ -32,16 +50,27 @@ int nextTrampID = 0;
 Semaphore semaphoreList[MAXSEMS];
 int curSem = 0;
 
+/*
+ * The lock() function gains the lock for user mode operations.
+ */
 void lock() {
     // Acquire the user mode lock:
     MboxRecv(userModeMBoxID, NULL, 0);
 }
 
+/*
+ * The unlock() function releases the lock for user mode operations.
+ */
 void unlock() {
     // Release the user mode lock:
     MboxSend(userModeMBoxID, NULL, 0);
 }
 
+/*
+ * The userModeTrampoline() function accepts a trampolineFunc pointer and
+ * runs its function in user mode. A pointer to this function is passed
+ * to spork() to create a process that runs a user-mode function.
+ */
 int userModeTrampoline(void *arg) {
     // Set to user mode:
     int old_psr = USLOSS_PsrGet();
@@ -63,6 +92,10 @@ int userModeTrampoline(void *arg) {
     assert(0);
 }
 
+/*
+ * The spawnSyscall() function represents the spawn syscall called in user mode.
+ * It calls spork on a user-mode process in user mode, creating a new process.
+ */
 void spawnSyscall(USLOSS_Sysargs *args) {
     lock();
 
@@ -95,6 +128,10 @@ void spawnSyscall(USLOSS_Sysargs *args) {
     unlock();
 }
 
+/*
+ * The waitSyscall() function represents the wait syscall called in user mode.
+ * It calls join on a user-mode process in user mode, waiting until it terminates.
+ */
 void waitSyscall(USLOSS_Sysargs *args) {
     lock();
 
@@ -117,6 +154,10 @@ void waitSyscall(USLOSS_Sysargs *args) {
     unlock();
 }
 
+/*
+ * The terminateSyscall() function represents the terminate syscall called in user mode.
+ * It calls quit on a user-mode process in user mode once all its children have joined.
+ */
 void terminateSyscall(USLOSS_Sysargs *args) {
     lock();
 
@@ -134,9 +175,17 @@ void terminateSyscall(USLOSS_Sysargs *args) {
     quit(quitStatus);
 }
 
+/*
+ * The semCreateSyscall() function represents the semCreate syscall called in user mode.
+ * It creates a new semaphore with a default value, returning its ID.
+ */
 void semCreateSyscall(USLOSS_Sysargs *args) {
     int semID;
+
+    // Call the kernel-mode semCreate:
     int result = kernSemCreate((int)(long)args->arg1, &semID);
+
+    // Set its return values:
     if (result == 0) {
         args->arg1 = (void *)(long)semID;
         args->arg4 = 0;
@@ -145,8 +194,16 @@ void semCreateSyscall(USLOSS_Sysargs *args) {
     }
 }
 
+/*
+ * The semPSyscall() function represents the semP syscall called in user mode.
+ * It decrements the accepted semaphore ID; if its value is 0, it blocks until
+ * the value is non-zero.
+ */
 void semPSyscall(USLOSS_Sysargs *args) {
+    // Call the kernel-mode semP:
     int result = kernSemP((int)(long)args->arg1);
+
+    // Set its return values:
     if (result == 0) {
         args->arg4 = 0;
     } else {
@@ -154,8 +211,16 @@ void semPSyscall(USLOSS_Sysargs *args) {
     }
 }
 
+/*
+ * The semVSyscall() function represents the semV syscall called in user mode.
+ * It increments the accepted semaphore ID, unblocking a waiting process
+ * if there are any.
+ */
 void semVSyscall(USLOSS_Sysargs *args) {
+    // Call the kernel-mode semV:
     int result = kernSemV((int)(long)args->arg1);
+
+    // Set its return values:
     if (result == 0) {
         args->arg4 = 0;
     } else {
@@ -163,14 +228,19 @@ void semVSyscall(USLOSS_Sysargs *args) {
     }
 }
 
-// FIXME Kern funs added so it compiles. Unsure what needs to be done for these.
+/*
+ * The kernSemCreate() function represents a kernel-mode version of semCreate,
+ * which creates a new semaphore and returns its ID.
+ */
 int kernSemCreate(int value, int *semaphore) {
     lock();
+
     // If out of semaphores or invalid input, return error:
     if (curSem >= MAXSEMS || value < 0) {
         unlock();
         return -1;
     }
+
     // Set up the new semaphore:
     semaphoreList[curSem].value = value;
     semaphoreList[curSem].numBlockedProcs = 0;
@@ -182,28 +252,33 @@ int kernSemCreate(int value, int *semaphore) {
     return 0;
 }
 
+/*
+ * The kernSemP() function represents a kernel-mode version of semP,
+ * which decrements a semaphore.
+ */
 int kernSemP(int semaphore) {
     lock();
+
     // Validate the semaphore ID (arg1):
     if (semaphore < 0 || semaphore >= curSem) {
         unlock();
         return -1;
     }
+
     // Get the semaphore to P(). If counter is zero, block until non-zero:
     if (semaphoreList[semaphore].value == 0) {
         int mbox = MboxCreate(0, 0);
         int back = semaphoreList[semaphore].back;
+
         // Add process to block queue to keep track of the order in which processes should unblock later:
         semaphoreList[semaphore].blockedMbox[back] = mbox;
         semaphoreList[semaphore].back = (back + 1) % MAXMBOX;
         semaphoreList[semaphore].numBlockedProcs++;
-        // release the lock
+
+        // Block until this semaphore gets V()ed, then release the new mailbox:
         unlock(); 
-        // Block until this semaphore gets V()ed:
         MboxSend(mbox, NULL, 0);
-        // release the mailbox so we don't take up space
         MboxRelease(mbox); 
-        // reacquire the lock
         lock(); 
     }
     // Decrement the semaphore value:
@@ -213,26 +288,34 @@ int kernSemP(int semaphore) {
     return 0;
 }
 
+/*
+ * The kernSemV() function represents a kernel-mode version of semV,
+ * which increments a semaphore.
+ */
 int kernSemV(int semaphore) {
     lock();
+
     // If invalid input, return error:
     if (semaphore < 0 || semaphore >= curSem) {
         unlock();
         return -1;
     }
-    // increment value
+
+    // Increment the semaphore value:
     semaphoreList[semaphore].value++;
-    // if any P()s are blocked, unblock one
+
+    // If any P()s are blocked, unblock one:
     if (semaphoreList[semaphore].numBlockedProcs > 0) {
         // Get the next semaphore and increment it:
         int front = semaphoreList[semaphore].front;
-        // get the mailbox
+
+        // Get the mailbox:
         int mbox = semaphoreList[semaphore].blockedMbox[front];
         semaphoreList[semaphore].front = (front + 1) % MAXMBOX;
         semaphoreList[semaphore].numBlockedProcs--;
 
+        // Wake up the blocked process:
         unlock();
-        // wake up blocked process
         MboxRecv(mbox, NULL, 0);
         lock();
     }
@@ -241,21 +324,36 @@ int kernSemV(int semaphore) {
     return 0;
 }
 
+/*
+ * The getTimeOfDaySyscall() function represents the getTimeOfDay syscall called in user mode.
+ * It returns the current time.
+ */
 void getTimeOfDaySyscall(USLOSS_Sysargs *args) {
     // Return the current time of day:
     args->arg1 = (void *)(long)currentTime();
 }
 
+/*
+ * The getPidSyscall() function represents the getPid syscall called in user mode.
+ * It returns the PID of the currently running process.
+ */
 void getPidSyscall(USLOSS_Sysargs *args) {
     // Return the current process' PID:
     args->arg1 = (void *)(long)getpid();
 }
 
+/*
+ * The dumpProcessesyscall() function represents the dumpProcesses syscall called in user mode.
+ * It prints basic information about every active process.
+ */
 void dumpProcessesSyscall() {
     // Print process information:
     dumpProcesses();
 }
 
+/*
+ * The phase3_init() function initializes data for the phase3 file.
+ */
 void phase3_init() {
     // Initialize every function in the syscall vec:
     systemCallVec[SYS_SPAWN] = (void (*)(USLOSS_Sysargs *))spawnSyscall;
@@ -292,6 +390,9 @@ void phase3_init() {
     }
 }
 
+/*
+ * TODO
+ */
 void phase3_start_service_processes() {
     // Deliberately left blank.
     // TODO Should it be deliberately left blank?
